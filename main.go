7eff78454b9b8eeb5a7ecf58289a4de738377095
Jose <jose.c.naylor@gmail.com>
2023-11-01 21:26:01 -0400

    Added endpoints and basic handler for projects and thoughts webpages

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"net/http"
 )
 
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello World!")
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, this is the landing page of my personal website!")
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, this is a page listing my projects!")
+}
+
+func thoughtsHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, this is a page for putting my thoughts on a page!")
 }
 
 func main() {
-    http.HandleFunc("/", helloWorldHandler)
+    http.HandleFunc("/", homePageHandler)
+    http.HandleFunc("/projects", projectsHandler)
+    http.HandleFunc("/thoughts", thoughtsHandler)
+    log.Printf("Server starting on port: 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
